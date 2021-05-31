cca8e849d06d8a42221c13b58dcc2c34b1215111
Arvinth-s <arvinth.saravana@gmail.com>
2021-05-31 09:05:58 +0530

    decode string #100q #stack #medium faster than 100%

---
+++ decode_string.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> st;
+        stack<int> val;
+        st.push("");
+        bool last_int=false;
+        for(char ch:s){
+            
+            
+            if(ch-'0' <= 9){
+                if(last_int){
+                    val.top()=val.top()*10+ch-'0';
+                }else{
+                    val.push(ch-'0');
+                }
+            
+                
+                last_int = true;
+                
+            }else if(ch=='[' || ch==']'){ 
+                if(ch==']'){
+                    string str_temp="";
+                    int val_temp=val.top();
+                    val.pop();
+                    for(int i=0;i<val_temp; i++)str_temp += st.top();
+                    st.pop();
+                    
+                    if(st.empty()){
+                        st.push(str_temp);
+                    }else{
+                        st.top() += str_temp;
+                    }
+                }else{
+                    st.push("");
+                }
+                
+                
+                last_int = false;
+
+            }else{
+                st.top().push_back(ch);
+                last_int = false;
+            }
+        }
+        
+        return st.top();
+    }
+};
